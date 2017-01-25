0f232c344d9210a2f94ae03d0f24a0960cbd7a93
Will Lewis <will@wflewis.com>
2017-01-25 18:56:35 -0500

    Add listing functionality

--- Main.hs
+++ Main.hs
@@ -1,4 +1,33 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import System.Process
+import System.Directory
+import System.IO.Unsafe
+
+(|>) = flip($)
+
+main =
+  "/Users/will"
+  |> ls
+  |> stdout
+
+stdout message =
+  message
+  |> lines
+  |> map ("echo " ++)
+  |> foldl (\a b -> a ++ b ++ "\n") ""
+  |> putStrLn
+
+cd new_dir =
+  "cd "
+  |> (++) new_dir
+  |> putStrLn
+
+projects_dir =
+  getHomeDirectory
+  |> unsafePerformIO
+  |> (++) "/Documents/projects"
+
+ls filter =
+  readProcess "ls" [filter] []
+  |> unsafePerformIO
