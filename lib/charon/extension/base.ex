e70e04ec50664f5361adf1afe90db702d914df69
Will Lewis <will@wflewis.com>
2017-05-07 18:19:29 -0400

    Fix previous commit

--- lib/charon/extension/base.ex
+++ lib/charon/extension/base.ex
@@ -10,24 +10,20 @@ defmodule Charon.Extension.Base do
   end
 
   def help(_args \\ []) do
-    "Usage: charon [command] [project name] [options]"
-    |> newline
-    |> newline |> concat("Base:")
-    |> newline |> concat("  version")
-    |> newline |> concat("  help")
-    |> newline |> concat("  debug")
-    |> newline
-    |> newline |> concat("Projects:")
-    |> newline |> concat("  list")
-    |> newline |> concat("  new")
-    |> newline |> concat("  destroy")
-    |> newline
-    |> newline |> concat("Git: ")
-    |> newline |> concat("  clone")
-    |> newline |> concat("  init")
-    |> stdout
+    "Usage: charon [command] [project name] [options]" |> stdout
+    ""            |> stdout
+    "Base:"       |> stdout
+    "├── version" |> stdout
+    "├── help"    |> stdout
+    "└── debug"   |> stdout
+    ""            |> stdout
+    "Projects:"   |> stdout
+    "├── list"    |> stdout
+    "├── new"     |> stdout
+    "└── destroy" |> stdout
+    ""            |> stdout
+    "Git:"        |> stdout
+    "├── clone"   |> stdout
+    "└── init"    |> stdout
   end
-
-  def newline(s), do: s ++ "\n"
-  def concat(s1,s2), do: s1 ++ s2
 end
